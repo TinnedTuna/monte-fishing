211ef0573d463d4df1391ce31146a7a09bd40c86
Dan Turner <dan.turner@york.ac.uk>
2013-04-06 16:09:14 +0100

    Minor typo

--- monte_fishing/main.py
+++ monte_fishing/main.py
@@ -2,8 +2,7 @@ import arbiter
 
 if __name__=='__main__':
     for i in range(4):
-        a = arbiter.Arbiter()
-        a.set_trials(500)
+        a = arbiter.Arbiter(500)
         a.start() # Non-blocking
         
 
