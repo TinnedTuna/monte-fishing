b69a063a1a469bbe1f8e3afaa841dc77fd349fe6
Dan Turner <dan.turner@york.ac.uk>
2013-04-06 16:42:26 +0100

    Main waits for the processes to finish.

--- monte_fishing/main.py
+++ monte_fishing/main.py
@@ -1,8 +1,12 @@
 import arbiter
 
 if __name__=='__main__':
+    jobs = []
     for i in range(4):
         a = arbiter.Arbiter(500)
         a.start() # Non-blocking
+        jobs.append(a)
+    for j in jobs:
+        j.join()
         
 
