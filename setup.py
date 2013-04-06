80b5b03f62d9065c47c77eaa250322f9485e84ae
Dan Turner <dan.turner@york.ac.uk>
2013-04-06 15:58:43 +0100

    Switched away from disttools

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+
+requires = [
+        'straight.plugin',
+        'nose',
+        ]
 
 setup(name='monte_fishing',
       version='0.0',
       packages=['monte_fishing'],
+      test_suite='monte_fishing.tests',
+      install_requires=requires
      )
