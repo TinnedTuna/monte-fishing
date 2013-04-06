fe96b90838c64fabc54dfcee98dc9276b71275ca
Dan Turner <dan.turner@york.ac.uk>
2013-04-06 13:12:10 +0100

    The deck now works as expected.

--- monte_fishing/deck.py
+++ monte_fishing/deck.py
@@ -13,11 +13,24 @@ class Card():
     def __repr__(self):
         return "Card:{Suit:"+self.suit+" value:"+str(self.value)+"}"
 
+    def __eq__(self,other):
+        return (other.suit == self.suit and other.value == self.value) 
+
+    def __ne__(self,other):
+        return not (other.suit == self.suit and other.value == self.value) 
+
+    def __hash__(self):
+        h = 5;
+        incl = 7;
+        h = h * self.suit.__hash__() + incl;
+        h = h * self.value.__hash__() + incl;
+        return h
+
 class Deck():
     """Represents a deck.
     """
-    deck = []
     def __init__(self):
+        self.deck = []
         for suit in ['Hearts','Spades','Diamonds','Clubs']:
             for value in range(1,14):
                 self.deck.append(Card(suit,value))
@@ -31,9 +44,9 @@ class Deck():
         return card
 
     def get_hand(self, size):
-        assert type(size) is long
+        assert (type(size) is long) or (type(size) is int)
         hand = []
-        for i in range(i):
-            hand.append(get_card())
+        for i in range(size):
+            hand.append(self.get_card())
         return hand
     
--- monte_fishing/tests/deck_tests.py
+++ monte_fishing/tests/deck_tests.py
@@ -1,18 +1,52 @@
 import unittest
 
-from monte_fishing.deck import Deck
+from monte_fishing.deck import (
+      Deck,
+      Card,
+      )
 
 class DeckTests(unittest.TestCase):
     def setUp(self):
-        pass
-
+        self.d = Deck()
+        
     def tearDown(self):
         pass
 
     def testDeckCreation(self):
-        d = Deck()
-        self.assertTrue(len(d.deck) == 52)
+        self.assertEqual(len(self.d.deck), 52)
+
+    def testGetCardDeckSize(self):
+        self.assertEqual(len(self.d.deck), 52)
+        c = self.d.get_card()
+        self.assertEqual(len(self.d.deck), 51)
 
     def testGetCard(self):
-        d = Deck()
-        self.assertTrue(d.get_card() not in d.deck)
+        self.assertEqual(len(self.d.deck), 52)
+        self.assertTrue(self.d.get_card() not in self.d.deck)
+   
+    def testGetHand(self):
+        hand = self.d.get_hand(7)
+        self.assertTrue(len(hand), 6)
+       
+    def testGetHandNoDuplicates(self):
+        hand = self.d.get_hand(52)
+        no_dupes_hand = set(hand)
+        self.assertTrue(len(no_dupes_hand), 52)
+        
+
+
+class CardTests(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    
+    def testEquality(self):
+        c_one = Card('Hearts',1)
+        c_two = Card('Hearts',1)
+        self.assertEqual(c_one, c_two)
+
+    def testDisequal(self):
+        c_one = Card('Hearts',1)
+        c_two = Card('Hearts',2)
+        self.assertNotEqual(c_one, c_two)
